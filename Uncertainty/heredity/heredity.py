f372ac8d07bc24162a765cf14a5538b9a9d408ad
etuckerman <elliot.tuckerman@gmail.com>
2024-07-17 14:13:51 -0400

    started implementing joint_probability()

--- Uncertainty/heredity/heredity.py
+++ Uncertainty/heredity/heredity.py
@@ -139,7 +139,79 @@ def joint_probability(people, one_gene, two_genes, have_trait):
         * everyone in set `have_trait` has the trait, and
         * everyone not in set` have_trait` does not have the trait.
     """
-    raise NotImplementedError
+    
+    #create a dict for the total probability
+    total_prob = {}
+    
+    #create set for people who have no gene
+    no_gene = set()
+    
+    #create set for people who have no trait
+    no_trait = set()
+    
+    
+    print(people, "people")
+    print(one_gene, "one_gene")
+    print(two_genes, "two_genes")
+    print(have_trait, "have_trait")
+    
+    
+    
+    for person in people:
+        mother = people[person]["mother"]
+        father = people[person]["father"]
+        print(f"{person}'s mother is {mother}")
+        print(f"{person}'s father is {father}")
+        
+        #everyone in set `one_gene` has one copy of the gene
+        if person in one_gene:
+            total_prob[person] += PROBS["gene"][1]
+        #everyone in set `two_genes` has two copies of the gene
+        elif person in two_genes:
+            total_prob[person] += PROBS["gene"][2]
+        else:
+            total_prob[person] += PROBS["gene"][0]
+        
+        #unconditional probability distribution
+        #For anyone with no parents listed in the data set,
+        if mother and father == None:
+            #use the probability distribution PROBS["gene"] 
+            # to determine the probability that they have a 
+            # particular number of the gene.
+            total_prob[person] += PROBS["gene"]
+            
+        #For anyone with parents in the data set
+        else:
+            #each parent will pass one of their 
+            # two genes on to their child randomly
+            
+        
+        #conditional probability distribution
+        if person not in have_trait:
+            if person in one_gene:
+                total_prob[person] += PROBS["trait"][1]
+            elif person in two_genes:
+                total_prob[person] += PROBS["trait"][2]
+            else:
+                total_prob[person] += PROBS["trait"][0]
+        
+        if 
+            
+        
+        
+        
+        #if both mother and father have no trait
+        if people[mother]["trait"] and people[father]["trait"] == True:
+            no_trait.add(person)
+        
+        
+        #if both mother and father have no gene
+        if people[mother][""]
+        
+            
+    
+    
+    #raise NotImplementedError
 
 
 def update(probabilities, one_gene, two_genes, have_trait, p):
