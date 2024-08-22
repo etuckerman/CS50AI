ef42da94d8b0a9da5fc7b1c51adbd4d6c6c6f7da
etuckerman <elliot.tuckerman@gmail.com>
2024-08-22 18:17:44 -0400

    ~applied style50

--- Learning/shopping/shopping.py
+++ Learning/shopping/shopping.py
@@ -63,24 +63,25 @@ def load_data(filename):
     labels should be the corresponding list of labels, where each label
     is 1 if Revenue is true, and 0 otherwise.
     """
-    month_dict = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
-    
+    month_dict = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'June',
+                  'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
+
     df = pd.read_csv(filename)
     print(df.head(10))
-    
+
     # Split DataFrame into 'evidence' and 'labels'
-    
+
     #  selects all columns except the last one.
     evidence = df.iloc[:, :-1]
-    
+
     #  selects the last column.
     labels = df.iloc[:, -1]
-    
+
     print(evidence.head(10))
     print(labels.head(10))
-    
-    #Administrative, Informational, ProductRelated, Month, 
-    # OperatingSystems, Browser, Region, TrafficType, VisitorType, 
+
+    # Administrative, Informational, ProductRelated, Month,
+    # OperatingSystems, Browser, Region, TrafficType, VisitorType,
     # and Weekend should all be of type int
     evidence['Administrative'] = evidence['Administrative'].astype(int)
     evidence['Informational'] = evidence['Informational'].astype(int)
@@ -90,12 +91,13 @@ def load_data(filename):
     evidence['Browser'] = evidence['Browser'].astype(int)
     evidence['Region'] = evidence['Region'].astype(int)
     evidence['TrafficType'] = evidence['TrafficType'].astype(int)
-    evidence['VisitorType'] = evidence['VisitorType'].apply(lambda x: 1 if x == 'Returning_Visitor' else 0)
+    evidence['VisitorType'] = evidence['VisitorType'].apply(
+        lambda x: 1 if x == 'Returning_Visitor' else 0)
     evidence['Weekend'] = evidence['Weekend'].apply(lambda x: 1 if x == True else 0)
-    
-    #Administrative_Duration, Informational_Duration, ProductRelated_Duration,
+
+    # Administrative_Duration, Informational_Duration, ProductRelated_Duration,
     # BounceRates, ExitRates, PageValues, and SpecialDay should all be of type float.
-    
+
     evidence['Administrative_Duration'] = evidence['Administrative_Duration'].astype(float)
     evidence['Informational_Duration'] = evidence['Informational_Duration'].astype(float)
     evidence['ProductRelated_Duration'] = evidence['ProductRelated_Duration'].astype(float)
@@ -103,18 +105,18 @@ def load_data(filename):
     evidence['ExitRates'] = evidence['ExitRates'].astype(float)
     evidence['PageValues'] = evidence['PageValues'].astype(float)
     evidence['SpecialDay'] = evidence['SpecialDay'].astype(float)
-    
-    #Each value of labels should either be the integer 1, if the user did go through with a purchase, or 0 otherwise.
+
+    # Each value of labels should either be the integer 1, if the user did go through with a purchase, or 0 otherwise.
     labels = labels.apply(lambda x: 1 if x == True else 0)
-    
+
     print(evidence.head(10))
     print(labels.head(10))
-    #print the first row of evidence
+    # print the first row of evidence
     print(evidence.iloc[0])
-    #print the first value of labels
+    # print the first value of labels
     print(labels.iloc[0])
-    return(evidence, labels)
-    
+    return (evidence, labels)
+
 
 def train_model(evidence, labels):
     """
@@ -123,13 +125,14 @@ def train_model(evidence, labels):
     """
     k = 1
 
-    #create test and train data and determine test train split
-    X_train, X_test, y_train, y_test = train_test_split(evidence, labels, test_size = TEST_SIZE, random_state = 42)
-    
-    knn = KNeighborsClassifier(n_neighbors = k)
-    
+    # create test and train data and determine test train split
+    X_train, X_test, y_train, y_test = train_test_split(
+        evidence, labels, test_size=TEST_SIZE, random_state=42)
+
+    knn = KNeighborsClassifier(n_neighbors=k)
+
     knn.fit(X_train, y_train)
-    
+
     return knn
 
 
@@ -151,27 +154,24 @@ def evaluate(labels, predictions):
     # Convert predictions and labels to pandas Series
     labels = pd.Series(labels)
     predictions = pd.Series(predictions)
-    
+
     # Calculate True Positives, False Negatives, True Negatives, and False Positives
     true_positives = ((labels == 1) & (predictions == 1)).sum()
     false_negatives = ((labels == 1) & (predictions == 0)).sum()
     true_negatives = ((labels == 0) & (predictions == 0)).sum()
     false_positives = ((labels == 0) & (predictions == 1)).sum()
 
+    # values of 1 are: true positives and false negatives
+    # values of 0 are: true negatives and false positives
 
-    #values of 1 are: true positives and false negatives
-    #values of 0 are: true negatives and false positives
-    
     # Calculate Sensitivity and Specificity
-    #how accurate values of 1 are
+    # how accurate values of 1 are
     sensitivity = true_positives / (true_positives + false_negatives)
-    
-    #how accurate values of 0 are
+
+    # how accurate values of 0 are
     specificity = false_positives / (false_positives + true_negatives)
-    
-    return(sensitivity, specificity)
-    
-    
+
+    return (sensitivity, specificity)
 
 
 if __name__ == "__main__":
